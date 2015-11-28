f459756048d084b7c03f717a7ba3ff77064abe54
Daisuke Baba <baba.daisuke@gmail.com>
2015-11-28 23:18:44 +0900

    Add initial files except ES5 files

---
+++ Gruntfile.js
@@ -0,0 +1,118 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  require('time-grunt')(grunt);
+
+  var config = {
+
+    watch: {
+      babel: {
+        files: ['**/*.es6.js'],
+        tasks: ['babel']
+      },
+      express: {
+        files:  ['**/*.js', '!**/*.es6.js', 'bin/www'],
+        tasks:  ['express:dev'],
+        options: {
+          livereload: true,
+          spawn: false
+        }
+      },
+      staticFiles: {
+        files: ['public/**/*.css', 'public/**/*.js'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+
+    express: {
+      options: {
+        port: 3000,
+        debug: true
+      },
+      dev: {
+        options: {
+          script: 'bin/www'
+        }
+      }
+    },
+
+    babel: {
+      options: {
+        plugins: ['uglify:after'],
+        sourceMap: true
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: './',
+          src: '**/*.es6.js',
+          dest: './',
+          ext: '.js'
+        }]
+      }
+    },
+    
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '**/*.map',
+            './*.tgz'
+          ]
+        }]
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        '**/*.js'
+      ]
+    },
+    mochaTest: {
+      all: {
+        options: {
+          require: 'babel/register'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+  };
+    
+  grunt.initConfig(config);
+  
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-express-server');
+
+  grunt.registerTask('test', [
+    'babel',
+    'jshint',
+    'mochaTest'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean',
+    'babel',
+  ]);
+
+  grunt.registerTask('default', [
+    'test',
+    'build'
+  ]);
+
+  grunt.registerTask('serv', [
+    'express:dev',
+    'watch'
+  ]);
+};
---
+++ README.md
@@ -0,0 +1,43 @@
+express + babel boilerplate
+===
+
+express + babel web application boilerplate running on Node.js v0.12+.
+
+`grunt build` will transpile all *.es6.js files into *.js with source maps. The generated files are placed at the same place as their ES6 sources.
+
+# Required Node.js Version
+
+Node.js v0.12+
+
+# Prepare Development
+
+    $ npm install -g grunt-cli babel mocha jshint
+    $ cd express-babel-boilerplate
+    $ npm install
+
+# How to build & debug
+
+    $ grunt build
+    $ npm run start
+
+# Server Operations
+
+## Development
+
+    $ grunt serv
+
+## Run in production
+
+    $ NODE_ENV=production npm run start
+
+# How to test & jshint
+
+    $ grunt test
+
+# Troubleshooting
+
+## "Fatal error: Port 35729 is already in use by another process."
+
+For Bash,
+
+    $ killall -9 grunt
