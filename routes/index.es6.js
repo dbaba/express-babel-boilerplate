cf98f4b042c83785f6b42e7a0034f11951fa6a47
Daisuke Baba <baba.daisuke@gmail.com>
2015-11-29 11:38:43 +0900

    Add ES6 source code

---
+++ routes/index.es6.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import 'source-map-support/register';
+import express from 'express';
+
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' });
+});
+
+export default router;
---
+++ routes/users.es6.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import 'source-map-support/register';
+import express from 'express';
+
+let router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  res.send('respond with a resource');
+});
+
+export default router;
